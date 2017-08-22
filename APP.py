873c033097bb4c5bf094c10e4eb3fd8246fbaf41
Bison-llc <30240683+Bison-llc@users.noreply.github.com>
2017-08-22 14:10:33 -0400

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,123 @@
+
+
+
+
+num-of-user = 0 
+finalusers = int(raw_input("dwse teliko arithmo: "))
+int(days) = 0
+
+
+Users = {}
+
+
+
+class users:
+    User.name = ''
+    User.wallet = 
+    user.xp = 
+    user.level = 
+    user.types = 
+    
+
+UProducts = users()
+UProducts.name = raw_input("Name of product: ")
+UProducts.category = int(raw_input("Define category of product: "))
+UProducts.wallet = 
+UProducts.xp = 
+UProducts.level = 
+UProducts.types = 
+
+
+BProducts = users()
+BProducts.category = int(raw_input("define category of product you want to borrow: "))
+BProducts.Name = raw_input("Name of product to borrow: ")
+BProducts.Price = 
+BProducts.days = 
+
+
+LProducts = users()
+LProducts.category = int(raw_input("define category of products you want to lend: "))
+LProducts.name = raw_input("Name of product to lend: ")
+LProducts.price = 
+LProducts.days = 
+
+
+
+
+
+
+
+
+
+finalusers = int(raw_input("give number of users: ")
+print(finalusers)
+
+
+# main
+
+nextday = int(raw_input("Enter for next day: "))
+
+
+
+if num-of-users <= finalusers:
+    print(int(raw_input("you want new user?: ")
+else:
+    print("max number of users")
+
+# upload product
+
+
+while True:
+    u = raw_input("Anyone wants to upload item? : ")
+    if u =
+    
+    
+    
+    
+    
+    
+    
+# user borrows
+    
+
+print(raw_input("give borrows user: "))
+
+
+danisths = raw_input("give lends user: ")
+danizomenos = raw_input("give borrows user: ")
+
+print(raw_input("Anyone wants to borrow a product? : "))
+
+
+# print "what is your name?"
+# name = raw_input()
+# print  "Hello %s!" % name
+
+
+product = 0
+
+while (product <= 35):
+    print "thelei na anevasei kapoios kati?"
+    product = raw_input()
+    print "%s" % product
+    product = product + 1
+else:
+    break
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+    
