de199c7975a8f0c3b783992f6898a42d4802d599
Bison-llc <30240683+Bison-llc@users.noreply.github.com>
2017-08-27 21:20:13 +0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,77 +1,91 @@
-num-of-user = 0 
+import sys
+import struct
+
+
+
+
+users = 5
 finalusers = int(raw_input("dwse teliko arithmo: "))
-int(days) = 0
+days = 0
 wallet = []
 xp = []
 Users = {}
-level = {}
+level = []
 types = []
-xilop
 
 
-Users = {}
-
-
-
-class users:
-    User.name = ''
-    User.wallet = 
-    user.xp = 
-    user.level = 
-    user.types = 
+class users:      
+      name = ""
+      wallet = 0
+      xp = 83
+      level = 1
+      types = ""
+u1 = users()
+u1.name = raw_input("Enter name of user: ")
+u1.wallet = ""
+u1.xp = ""
     
 
 UProducts = users()
 UProducts.name = raw_input("Name of product: ")
-UProducts.category = int(raw_input("Define category of product: "))
-UProducts.wallet = 
-UProducts.xp = 
-UProducts.level = 
-UProducts.types = 
+UProducts.category = raw_input("Define category of product: ")
+UProducts.wallet = raw_input("Define points: ")
+UProducts.xp = raw_input("Define xp: ")    
+UProducts.level = raw_input("Define level: ")
+UProducts.types = raw_input("define type of user: ")
 
 
 BProducts = users()
 BProducts.category = int(raw_input("define category of product you want to borrow: "))
 BProducts.Name = raw_input("Name of product to borrow: ")
-BProducts.Price = 
-BProducts.days = 
+BProducts.Price = int(raw_input(" "))
+BProducts.days = raw_input(" ")
 
 
 LProducts = users()
 LProducts.category = int(raw_input("define category of products you want to lend: "))
 LProducts.name = raw_input("Name of product to lend: ")
-LProducts.price = 
-LProducts.days = 
+LProducts.price = int(raw_input(" "))    
+LProducts.days = int(raw_input(" "))
 
 
-
-
-
-
-
-
-
-finalusers = int(raw_input("give number of users: ")
-print(finalusers)
-
+int (x = 0)
+if x < 34:
+      fu = int(raw_input("give number of users: "))
+      print(fu)
+else:
+      print("Max users")
 
 # main
 
 nextday = int(raw_input("Enter for next day: "))
-
-
-
-if num-of-users <= finalusers:
-    print(int(raw_input("you want new user?: ")
-else:
-    print("max number of users")
-
-# upload product
-
-
 while True:
     u = raw_input("Anyone wants to upload item? : ")
-    if u =
+
+
+#if num-of-users <= finalusers:
+#print(int(raw_input("you want new user?: ")
+#else:
+#print("max number of users")
+
+#upload product
+ 
+y = 0
+
+if y==0 :
+      u = str(raw_input("Anyone wants to upload item? : "))
+      if u == str("yes"):
+            name = raw_input("Give use name: ")
+            print(name)
+      else:
+            print("stoped")
+                
+else:
+      print(nothing)          
+                
+                
+                
+          
     
     
     
@@ -104,7 +118,15 @@ while (product <= 35):
     print "%s" % product
     product = product + 1
 else:
-    break
+    print("nothing")
+    
+    
+    
+    
+    
+    
+    
+#def main():
     
     
     
